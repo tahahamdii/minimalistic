130547ba90806a23231c6edfcd1bb67f09081f41
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-08-21 00:39:49 +0200

    fin journée

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,6 +108,21 @@ export default function Home() {
           ))}
         </div>
       </section>
+      <section id="contact">
+        <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 10}>
+            <div className="space-y-3">
+              <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                Contact
+              </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                Get in Touch
+              </h2>
+              
+            </div>
+          </BlurFade>
+        </div>
+      </section>
       
   
     </main>
