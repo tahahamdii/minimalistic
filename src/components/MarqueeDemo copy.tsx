9d4d392379e1d7b6bffbf9a4cf20cca4244fde88
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-08-23 03:15:23 +0200

    carousel left and right width reduced to 16%

--- src/components/MarqueeDemo copy.tsx
+++ src/components/MarqueeDemo copy.tsx
@@ -77,8 +77,8 @@ export function MarqueeDemo1() {
           <ReviewCard key={review.username} {...review} />
         ))}
       </Marquee> */}
-      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
-      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
+      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/6 bg-gradient-to-r from-white dark:from-background"></div>
+      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/6 bg-gradient-to-l from-white dark:from-background"></div>
     </div>
   );
 }
