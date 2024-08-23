32b0bf546386a6bfa07cc30fade2af4857cbf00e
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-08-23 03:12:18 +0200

    added two examles

--- src/components/MarqueeDemo.tsx
+++ src/components/MarqueeDemo.tsx
@@ -1,31 +1,44 @@
 import { cn } from "@/lib/utils";
 import Marquee from "@/components/magicui/marquee";
 const reviews = [
-    {
-      name: "Chamseddine Madouri",
-      username: "@chamseddine",
-      body: "Working with Taha has been such a pleasure! His enthusiasm and skill in AI are unmatched. Always ready with a smile and innovative ideas, he's the type of person who makes any project a success.",
-      img: "https://avatar.vercel.sh/chamseddine",
-    },
-    {
-      name: "Ahmed Chebbi",
-      username: "@ahmed",
-      body: "Taha is not only brilliant but also one of the most approachable and genuine people I've worked with. His ability to simplify complex problems while keeping the energy high is remarkable. I can't wait to collaborate again!",
-      img: "https://avatar.vercel.sh/ahmed",
-    },
-    {
-      name: "Imem Hamdi",
-      username: "@imem",
-      body: "Taha brings a rare mix of professionalism and warmth. His dedication to his work is inspiring, and he knows how to make every moment fun and productive. A true star in his field!",
-      img: "https://media.licdn.com/dms/image/v2/D4E35AQFYzUyQAbT-bw/profile-framedphoto-shrink_800_800/profile-framedphoto-shrink_800_800/0/1719666552589?e=1724965200&v=beta&t=gUyxkJMfdsIR07d4cp1OVwMbYaOYwcT3e7AiUArUOYk",
-    },
-    {
-      name: "Yousri Hamdene",
-      username: "@yousri",
-      body: "Taha is such a joy to work with! He's a true professional with an infectious positive attitude, and his ability to deliver results is top-notch. Anyone would be lucky to have him on their team.",
-      img: "https://avatar.vercel.sh/yousri",
-    }
-  ];
+  {
+    name: "Ahmed Chebbi",
+    username: "@ahmed",
+    body: "Working with Taha was fantastic! He quickly solved a major issue during a project, saving us a lot of time. His energy is truly IMPRESSIVE ",
+    img: "https://conceptmageg.com/uploads/images/202208/image_750x_62ee4e4be1383.jpg",
+  },
+  {
+    name: "Imem Hamdi",
+    username: "@imem",
+    body: "During a hackathon, Taha turned a challenging idea into a winning project in no time. He’s not only brilliant but also makes work enjoyable. Much Love!",
+    img: "https://media.licdn.com/dms/image/v2/D4E35AQFYzUyQAbT-bw/profile-framedphoto-shrink_800_800/profile-framedphoto-shrink_800_800/0/1719666552589?e=1724965200&v=beta&t=gUyxkJMfdsIR07d4cp1OVwMbYaOYwcT3e7AiUArUOYk",
+  },
+  {
+    name: "Yousri Hamdene",
+    username: "@yousri",
+    body: "I loved working with Taha he brings a lot of enthusiasm and skill to the table.",
+    img: "https://media.licdn.com/dms/image/v2/D4D03AQHeBnrYF0ojDQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1707048728617?e=1729728000&v=beta&t=WjbusZL6LpW81ONOpP6gcWb8oQ47RiiPhFaWP8LUhNI",
+  },
+  {
+    name: "Lina Ben Salah",
+    username: "@lina",
+    body: "love the creativity , true team player ",
+    img: "https://media.licdn.com/dms/image/v2/D4D03AQFF8LBBXYqnQw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1715549520205?e=1729728000&v=beta&t=1q4Vbgo15sdGFE6E_FxNXyyg4V2h4SWMMV2hVLXG9q4",
+  },
+  {
+    name: "Nadia Farhat",
+    username: "@nadia",
+    body: "Very impressive work , Big thanks for the hard work during our last project",
+    img: "https://media.licdn.com/dms/image/v2/C4E03AQGBK3UW7YbGug/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1654338067624?e=1729728000&v=beta&t=SwEnOYznkYjrO6OaOUYXB9mUBXsQatokP0scBbsLqaw",
+  },
+  {
+    name: "Omar Khedher",
+    username: "@omar",
+    body: "Taha’s enthusiasm and expertise made our last project a real success. He’s not only talented but also a great person to work with.",
+    img: "https://media.licdn.com/dms/image/v2/D4E03AQEsilSBOP4qdA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1724248539200?e=1729728000&v=beta&t=4PtEzPxZFrfr-713qMPG6ScKekRVWx9nGEbuhojq6Y4",
+  }
+];
+
 
 
 const firstRow = reviews.slice(0, reviews.length );
@@ -70,7 +83,7 @@ const ReviewCard = ({
 export function MarqueeDemo() {
   return (
     <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden  bg-inherit">
-      <Marquee pauseOnHover className="[--duration:20s]">
+      <Marquee pauseOnHover className="[--duration:24s]">
         {firstRow.map((review) => (
           <ReviewCard key={review.username} {...review} />
         ))}
