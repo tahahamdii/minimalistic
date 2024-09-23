b7ad3250c928a553df51f97bb404ba444e289f7f
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-09-23 02:14:31 +0200

    fix

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -11,7 +11,7 @@ export const DATA = {
   description:
     "Software Engineer / Junior entrepreneur. ",
   summary:
-    "it's me Taha from Tunis. I'm a final-year software engineering student at [**_ESPRIT_**](https://esprit.tn/).  I specialize in fullstack development and machine learning, recently interned [**_@Talan_**](https://www.linkedin.com/company/talan-tunisie/) for the summer, and i'm currently looking for **an end-of-study internship**, i'm highly collaborative and i always aim to bring my best to the team ! ",
+    "it's me Taha from Tunis. I'm a final-year software engineering student at [**_ESPRIT_**](https://esprit.tn/). Throughout my career, I have developed a strong foundation in the software development life cycle, including requirements gathering, design, implementation, and maintenance, recently interned [**_@Talan_**](https://www.linkedin.com/company/talan-tunisie/) for the summer, and i'm currently looking for **an end-of-study internship**, i'm highly collaborative and i always aim to bring my best to the team ! ",
   avatarUrl: "https://taha-bucket-v1.s3.amazonaws.com/newedit.png",
   skills: [
     "Spring Boot",
